166858bcc67aa658dd340c5024af1cfa93d93385
junglefive <2282918128@qq.com>
2017-03-27 20:39:37 +0800

    增加了部分菜单，增加了高亮（初步）

--- ChipseaConfiger/MainWindow.xaml.cs
+++ ChipseaConfiger/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace ChipseaConfiger
 {
@@ -21,47 +26,112 @@ namespace ChipseaConfiger
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        IHighlightingDefinition chipseaAssemblyHighlighting;
         public MainWindow()
         {
+           
             InitializeComponent();
-            TextEditorLeft.ShowLineNumbers = true;
-            TextEditorRight.ShowLineNumbers = true;
-            
+            IHighlightingDefinition highlighting = UserHighlightInitiate();
+            TextEditorLeft.SyntaxHighlighting = highlighting;
+            TextEditorRight.SyntaxHighlighting = highlighting;
+            TextEditorLeft.FontSize = 14;
+        }
+        private IHighlightingDefinition UserHighlightInitiate() {
+
+            // Load our custom highlighting definition
+           
+            using (Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream("ChipseaConfiger.Highlighting.ChipseaAssemblyHighlighting.xshd"))
+            {
+                if (s == null)
+                    throw new InvalidOperationException("Could not find embedded resource");
+                using (XmlReader reader = new XmlTextReader(s))
+                {
+                    chipseaAssemblyHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+            // and register it in the HighlightingManager
+            HighlightingManager.Instance.RegisterHighlighting("ChipseaAssembly Highlighting", new string[] { ".asm","inc" }, chipseaAssemblyHighlighting);
+            return chipseaAssemblyHighlighting;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             
         }
+
+
+        private void NewFileClick(object sender, RoutedEventArgs e) {
+
+
+        }
+        private void SaveFileClick(object sender, RoutedEventArgs e)
+        {
+            if (currentFileName == null)
+            {
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.DefaultExt = ".asm";
+                if (dlg.ShowDialog() ?? false)
+                {
+                    currentFileName = dlg.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            TextEditorLeft.Save(currentFileName);
+
+        }
+        string currentFileName;
+        private void OpenFileClick(object sender, RoutedEventArgs e) {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.CheckFileExists = true;
+            if (dlg.ShowDialog() ?? false)
+            {
+                currentFileName = dlg.FileName;
+                TextEditorLeft.Load(currentFileName);
+                TextEditorRight.Load(currentFileName);
+                TextEditorLeft.SyntaxHighlighting = HighlightingManager.Instance.GetDefinitionByExtension(System.IO.Path.GetExtension(currentFileName));
+            }
+
+        }
         private void ToggleLayout(object sender, RoutedEventArgs e) {
 
             if (this.GridPanel.ColumnDefinitions.Count > 1)
             {
                 this.GridPanel.ColumnDefinitions.Remove(ColumnRight);
+                LayoutSingleItem.IsChecked = true;
             }
             else {
                 this.GridPanel.ColumnDefinitions.Add(ColumnRight);
+                LayoutSingleItem.IsChecked = false;
             }
         }
         private void ToggleStatusBar(object sender, RoutedEventArgs e) {
-            if (this.GridWindow.RowDefinitions.Contains(RowStatusBar))
-            {
-                this.GridWindow.RowDefinitions.Remove(RowStatusBar);
+            if (StatusBarItem.IsChecked) {
+                RowStatusBar.Height = new GridLength(0);
+                StatusBarItem.IsChecked = false;
             }
-            else {
-                this.GridWindow.RowDefinitions.Add(RowStatusBar);
+            else{
+                StatusBarItem.IsChecked = true;
+                RowStatusBar.Height = new GridLength(20);
             }
+            
         }
         private void ToggleShowWordWrap(object sender, RoutedEventArgs e) {
             if (TextEditorLeft.WordWrap)
             {
                 TextEditorLeft.WordWrap = false;
                 TextEditorRight.WordWrap = false;
+                WordWrapItem.IsChecked = false;
             }
             else
             {
                 TextEditorLeft.WordWrap = true;
                 TextEditorRight.WordWrap = true;
+                WordWrapItem.IsChecked = true;
             }
 
 
@@ -72,16 +142,13 @@ namespace ChipseaConfiger
             {
                 TextEditorLeft.ShowLineNumbers = false;
                 TextEditorRight.ShowLineNumbers = false;
+                LineNumberItem.IsChecked = false;
             }
             else {
                 TextEditorLeft.ShowLineNumbers = true;
                 TextEditorRight.ShowLineNumbers = true;
+                LineNumberItem.IsChecked = true;
             }
-
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
 
         }
     }
